f96ba0cb90e81dd82a0337c117597b52b9d8a3b3
habibhardy <habibhardy10@gmail.com>
2023-08-24 21:57:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import { Navbar, Nav, Container, Card } from 'react-bootstrap';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'  >
+
+
+                  {/* Barre de navigation */}
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Mon Application</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Accueil</Nav.Link>
+                <Nav.Link href="#link">Lien</Nav.Link>
+               
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        {/* En-tête */}
+        <header className="App-header">
+          <h1>En tête de l'application</h1>
+        </header>
+
+      
+        <Container>
+          <div className="card-container">
+            <Card>
+              <Card.Body>
+                <Card.Title>Carte 1</Card.Title>
+                <Card.Text>Contenu de la carte 1.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Body>
+                <Card.Title>Carte 2</Card.Title>
+                <Card.Text>Contenu de la carte 2.</Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Body>
+                <Card.Title>Carte 3</Card.Title>
+                <Card.Text>Contenu de la carte 3.</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </Container>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
